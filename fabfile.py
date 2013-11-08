08bb1deabaaea0594c0cddc8e14e1eb681be1ae6
Marcus Bitzl <marcus@bitzl.com>
2013-11-08 18:26:10 +0100

    Added fabric build script to create an archive matching the standards of CTAN.

---
+++ fabfile.py
@@ -0,0 +1,33 @@
+from __future__ import print_function
+from zipfile import ZipFile
+from fabric.api import local
+from os import path
+
+class Package(ZipFile):
+    def __init__(self, name, folder):
+        ZipFile.__init__(self, name, 'w')
+        self.folder = folder
+    
+    def add(self, name, new_name=None):
+        if new_name == None:
+            new_name = name
+        self.write(name, path.join(self.folder, new_name))
+
+
+def zip(zipfile='test.zip'):
+    with Package(zipfile, 'grid-system') as zip:
+        zip.add('grid-system.pdf')
+        zip.add('grid-system.sty')
+        zip.add('grid-system.tex')
+        zip.add('README.md', 'README')
+        zip.add('LICENSE')
+
+
+def build_missing_pdf():
+    if not path.exists('grid-system.pdf'):
+        local('pdflatex grid-system')
+
+
+def default():
+    build_missing_pdf()
+    zip()
